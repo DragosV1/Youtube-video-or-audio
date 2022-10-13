1182fe9b00bd9ec695a4042fcbdf2ae7ffc5fa60
Dragos Vasile <105662674+DragosV1@users.noreply.github.com>
2022-10-13 16:03:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## This is a youtube video/audio downloader
+
+### Usage
+
+#### Install pytube
+
+
+
+```
+$> python -m pip install pytube
+```
+Create to folders: `audio` and `video`.
+After installation run `youtube_audio_or_video_download.py`. (You can name it as you want)
---
+++ youtube_audio_or_video_download.py
@@ -0,0 +1,32 @@
+from pytube import YouTube
+
+# https://www.youtube.com/watch?v=ijj_hheGEi0 - A link if you want to try
+
+link = input("Enter the link:\n>> ")
+yt = YouTube(link)
+
+print(f"Title: {yt.title}")
+print(f"Number of views: {yt.views}\n")
+
+user_input = input("This is the video you want do download? (y/n) ").lower()
+if user_input == "y":
+    audio_or_video = input("\nYou want the video(v) or audio(a)? (v/a) ").lower()
+    if audio_or_video == "a":
+        try:
+            print("\nDownloading audio...")
+            ys = yt.streams.get_audio_only()
+            ys.download("audio")
+            print(f"{yt.title} has been downloaded successfully.")
+        except:
+            print("Something went wrong! Try again or use another link.")
+    if audio_or_video == "v":
+        try:
+            print("\nDownloading video...")
+            ys = yt.streams.get_highest_resolution()
+            ys.download("video")
+            print(f"{yt.title} has been downloaded successfully.")
+        except:
+            print("Something went wrong! Try again or use another link.")
+
+if user_input == "n":
+    print("\nYou made the decision not do download.")
